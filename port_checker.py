30b7ced595ca235169f3cd2303a6fd0b421c94c6
julianmedwards <julian.michael.edwards@gmail.com>
2021-07-13 14:27:38 -0500

    Basic Setup
    Very first steps to start.

---
+++ port_checker.py
@@ -0,0 +1,15 @@
+# with open("example_saves/450.10.1_bosporan_kingdom.rome", 'r') as base_save:
+open("example_saves/451.2.2_bosporan_kingdom.rome", 'r')
+
+game/setup/provinces
+
+    # csv import
+# with open("data_files/steam_game_names.csv", 'r') as games_raw:
+#     rows = csv.reader(games_raw, delimiter = ',')
+#     games_list = []
+
+#     for r in rows:
+#         games_list.append(''.join(r))
+#     print(games_list[0 : 14])
+
+nation = "BPK"
