ced8ac26ed48055326e6bfe0e19a4d4a5f048947
julianmedwards <julian.michael.edwards@gmail.com>
2021-07-14 15:03:25 -0500

    Wrote reading and nation/port line finding.

--- port_checker.py
+++ port_checker.py
@@ -1,7 +1,4 @@
-# with open("example_saves/450.10.1_bosporan_kingdom.rome", 'r') as base_save:
-open("example_saves/451.2.2_bosporan_kingdom.rome", 'r')
-
-game/setup/provinces
+# game/setup/provinces
 
     # csv import
 # with open("data_files/steam_game_names.csv", 'r') as games_raw:
@@ -12,4 +9,43 @@ game/setup/provinces
 #         games_list.append(''.join(r))
 #     print(games_list[0 : 14])
 
-nation = "BPK"
+save_file = open("example_saves/451.2.2_bosporan_kingdom.rome", 'r')
+
+# Needs to be rewritten to search and recognize entries from the games'
+# nations files.
+test_nations = {'Bosporan Kingdom': 'BPK', 'Rome': 'ROM', 'Scythia': 'SCY'}
+
+nation_input = "Which nation's ports would you like to check? "
+nation = input(nation_input)
+
+if nation in test_nations:
+    save_tag = f'tag="{test_nations[nation]}"'
+    print(save_tag)
+else:
+    print('Nation not found.')
+
+flag = 0
+index = 0
+
+for line in save_file:
+    index += 1
+    
+    if save_tag in line:
+        flag = 1
+        save_nation_line = index
+        print(save_nation_line)
+    if flag == 1 and "ports={" in line:
+        save_ports_line = index
+        print(save_ports_line)
+
+        # Find space, skip space, read every digit until hitting and
+        # skipping another space, and so on, until reaching end bracket.
+
+        break
+
+if flag == 0:
+    print('\nTag for', nation, 'not found')
+else:
+    print('\nTag for', nation, 'found in line:', save_nation_line)
+
+save_file.close()
